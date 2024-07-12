83836cbd85811dffca9ad78f3371c1d660ac7446
AnjanaDeepak7 <anjanadeepak60@gmail.com>
2024-07-12 09:54:14 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./components/data";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let amount = parseInt(value);
+    if (value <= 1) {
+      amount = 1;
+    }
+    if (value >= data.length) {
+      amount = data.length;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Tired of boring lorem ipsum</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="paragraph" className="para-text">
+          Paragraph:{" "}
+        </label>
+        <input
+          type="number"
+          name="paragraph"
+          id="paragraph"
+          className="input"
+          value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <div className="para-container">
+        {text.map((item, index) => {
+          return (
+            <article key={index}>
+              <p className="text">{item}</p>
+            </article>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
